4874b0f92730148ae20d339ee79535c0d2edfae8
gobbedu <gobbolegal@gmail.com>
2021-11-06 20:36:22 -0300

    jsus

--- main.c
+++ main.c
@@ -1,1 +1,7 @@
-batata
+#include "tree.h"
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
--- tree.h
+++ tree.h
@@ -15,14 +15,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MUL 2147483647
-#define DIV 2147483646
-#define ADD -2147483646
-#define SUB -2147483647
-
-#define OPER_SZ 100
-
-
 struct nodoB 
 {
     struct nodoB *pai;
@@ -48,18 +40,6 @@ struct Arvore_B
     struct nodoB *raiz;
 };
 
-int opera(struct no *nodo);
-int resolve_arvore(struct tree *arvore, struct no *nodo);
-int temp(struct tree *arvore, struct no *nodo);
-void place_nodo(struct no *galho, struct no *folha);
-void cresce_arvore(struct tree *arvore, char* entrada);
-void planta_arvore(struct tree *arvore);
-int pai_de_folhas(struct no *nodo);
-int to_num(char *c, int i, int *diff);
-struct no* cria_nodo(void);
-int folha(struct no *nodo);
-int pai(struct no *nodo);
-void kill(char* msg, int sig);
 
 
 #endif
